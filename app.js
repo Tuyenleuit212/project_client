955cec8b89cbd3a61a506545d3d9e582d5ef6070
Mặt trăng khuyết <121482518+Tuyenleuit212@users.noreply.github.com>
2023-06-03 16:20:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const chatRouter = require('./routes/chatRoutes')
 const app = express()
 
 app.use(cors({
-    origin: "https://yourfrontendurl.com",
+    origin: "https://myway-shop-app.onrender.com",
     credentials:  true
 }));
 app.set("trust proxy",1);
