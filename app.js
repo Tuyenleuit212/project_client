2dd7446d8ecb21a4e5ab5e5b79b9e8c8725c7ba7
Mặt trăng khuyết <121482518+Tuyenleuit212@users.noreply.github.com>
2023-06-03 20:15:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const reviewRouter = require('./routes/reviewRoutes')
 const chatRouter = require('./routes/chatRoutes')
 const app = express()
 
-app.use(cors())
 app.use(express.static('public/image'));
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 app.use(mongoSanitize());
 app.get('/' , (req,res) => {
     res.status(200).json({
