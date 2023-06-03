3712d397b83de6e374fdfa49ca75510a3f38f1f5
Mặt trăng khuyết <121482518+Tuyenleuit212@users.noreply.github.com>
2023-06-03 21:27:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -20,13 +20,6 @@ app.use(cors({
     origin: 'https://myway-shop-app.onrender.com',
     credentials : true
 }));
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  next();
-});
 app.use(mongoSanitize());
 app.get('/' , (req,res) => {
     res.status(200).json({
