2fa057d3dcf606e1cc7e6bf0b9a4db6643ce32e2
Mặt trăng khuyết <121482518+Tuyenleuit212@users.noreply.github.com>
2023-06-03 20:17:53 +0700

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -16,9 +16,7 @@ const createSendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
-        secure: true,
-        samesite="none"
+        httpOnly: true
     };
     // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
